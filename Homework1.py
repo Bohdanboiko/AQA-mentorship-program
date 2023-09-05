54787c3e36c9e2b8bef939dcf6de2acd82b08426
Bohdanboiko <Bohdan.boiko.kn.2017@lpnu.uaBohdan.boiko.kn.2017@lpnu.uabohdan.boiko.kn.2017@lpnu.ua>
2023-09-05 14:09:57 +0300

    Add 4 homework

--- Homework1.py
+++ Homework1.py
@@ -1,15 +1,19 @@
+
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2, number):
+    if number != int(number):
+        return False  # Перевірка на цілість
+    for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
     return True
 
-
-num = int(input("Введіть число: "))
-
-if is_prime(num):
-    print(f"{num} є простим числом.")
-else:
-    print(f"{num} не є простим числом.")
+try:
+    num = float(input("Введіть число: "))
+    if is_prime(num):
+        print(f"{int(num)} є простим числом.")
+    else:
+        print(f"{int(num)} не є простим числом.")
+except ValueError:
+    print("Введене значення не є числом.")
--- Homework2.py
+++ Homework2.py
@@ -1,8 +1,10 @@
+
 def is_palindrome(string):
     clean_string = ''.join(char for char in string.lower() if char.isalnum())
-    return clean_string[0] == clean_string[-1]
+    return clean_string == clean_string[::-1]
 
 input_string = input("Введіть рядок: ")
+
 if is_palindrome(input_string):
     print("Рядок є паліндромом.")
 else:
